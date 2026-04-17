c33f474de25c727fdd827798f75b305ddd14ac5b
albacrespi <acrespi001@dundee.ac.uk>
2026-04-17 15:43:01 +0100

    copied css file inside each language

--- docs/es/source/conf.py
+++ docs/es/source/conf.py
@@ -76,7 +76,7 @@ html_theme_options = {
 html_logo = "../images/SATRE_Stacked_Dark.png"
 
 # Add custom CSS and JavaScript
-html_static_path = ["../_static"]
+html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 # html_js_files = ["custom.js"]
 
